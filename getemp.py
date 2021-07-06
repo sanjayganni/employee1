b5f3d21e303eaab61223b3e329b23c93958aa9bd
sanjayganni <86920619+sanjayganni@users.noreply.github.com>
2021-07-06 13:48:42 +0100

    Update and rename getemp.py.txt to getemp.py

--- getemp.py
+++ getemp.py
@@ -10,4 +10,4 @@ app = Flask(__name__)
 def home():
     return render_template('getemp.html')
 if __name__ =='__main__':
-		app.run(host='0.0.0.0',port=8080,debug=True)
+	app.run(host='0.0.0.0',port=8080,debug=True)
